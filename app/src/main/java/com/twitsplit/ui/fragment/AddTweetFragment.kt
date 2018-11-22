0926883115346c1af2872d9a12434aa81c893527
prakash.mundra <prakash.mundra@eres.ae>
2018-11-22 21:06:29 +0400

    Implemented adding new tweet with its fragment and viewmodel.

--- app/src/main/java/com/twitsplit/ui/fragment/AddTweetFragment.kt
+++ app/src/main/java/com/twitsplit/ui/fragment/AddTweetFragment.kt
@@ -1,9 +1,43 @@
 package com.twitsplit.ui.fragment
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.twitsplit.R
+import com.twitsplit.databinding.FragmentAddTweetBinding
+import com.twitsplit.viewmodel.AddTweetModel
 
 class AddTweetFragment : Fragment() {
     companion object {
         val TAG = AddTweetFragment::class.simpleName!!
+    }
+
+    private lateinit var mBinding: FragmentAddTweetBinding
+    private lateinit var mModel: AddTweetModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_tweet, container, false)
+        activity?.let {
+            val save = it.findViewById<FloatingActionButton>(R.id.save_tweet)
+            save.setOnClickListener {
+                if (mModel.saveTweet())
+                    activity!!.finish()
+            }
+        }
+        return mBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.let {
+            val factory = AddTweetModel.Factory(it.application)
+            mModel = ViewModelProviders.of(this, factory).get(AddTweetModel::class.java)
+            mBinding.viewmodel = mModel
+        }
     }
 }
--- app/src/main/java/com/twitsplit/viewmodel/AddTweetModel.kt
+++ app/src/main/java/com/twitsplit/viewmodel/AddTweetModel.kt
@@ -2,7 +2,42 @@ package com.twitsplit.viewmodel
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.databinding.ObservableField
+import android.widget.Toast
+import com.twitsplit.App
+import com.twitsplit.R
+import com.twitsplit.data.TweetsDataRepository
+import com.twitsplit.data.model.Tweet
 
-class AddTweetModel(application: Application) : AndroidViewModel(application) {
+class AddTweetModel(application: Application, private val repository: TweetsDataRepository) : AndroidViewModel(application) {
+    val tweet = ObservableField<String>()
 
+    fun saveTweet(): Boolean {
+        val tweet = tweet.get()
+        return if (tweet != null && tweet.trim().isNotEmpty()) {
+            val splits = tweet.split(" ")
+            if (tweet.length < 50 || (tweet.length > 50 && splits.size > 1)) {
+                val newTweet = Tweet(tweet = tweet)
+                repository.saveTweet(newTweet)
+                true
+            } else {
+                Toast.makeText(getApplication<Application>().baseContext,
+                        R.string.space_tweet_message, Toast.LENGTH_SHORT).show()
+                false
+            }
+        } else {
+            Toast.makeText(getApplication<Application>().baseContext,
+                    R.string.empty_tweet_message, Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    class Factory(private val application: Application) : ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            @Suppress("UNCHECKED_CAST")
+            return AddTweetModel(application, (application as App).getDataRepository()) as T
+        }
+    }
 }
